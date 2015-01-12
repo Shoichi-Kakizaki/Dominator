17ee6d6fbb758366b5c0c54bdc0576aaa8e1787e
kakizaki2 <s-kakizaki.basicinc.jp>
2015-01-12 19:42:08 +0900

    MainActivity スタートボタン配置

--- src/com/shoichi/dominator/MainActivity.java
+++ src/com/shoichi/dominator/MainActivity.java
@@ -1,9 +1,16 @@
 package com.shoichi.dominator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnLongClickListener;
+
+
 
 public class MainActivity extends Activity {
 
@@ -12,4 +19,13 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+		
+	}
+    public void startBtn(View v) {
+    	 Log.d("startBtn", "押された！？");
+     }
 }
